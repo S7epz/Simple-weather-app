2b21180a0165cfda08f229410583f9f7ba40e492
SePuede <stale4669@gmail.com>
2024-08-04 17:01:40 +0300

    Initial Commit

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,23 @@
+//
+// Created by S7epz on 04/08/24.
+// Copyright © 2024 ACME.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,16 @@
+//
+// Created by S7epz on 04/08/24.
+// Copyright © 2024 ACME.
+// All Rights Reserved.
+
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
